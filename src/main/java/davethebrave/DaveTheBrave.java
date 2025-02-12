df83cb32d9a6f909dd9c99e8244d6eb3ce66c24b
Jen999 <tanlingjen.tlj@gmail.com>
2025-02-12 20:54:37 +0800

    Integrate original chatbot functionality

--- src/main/java/davethebrave/DaveTheBrave.java
+++ src/main/java/davethebrave/DaveTheBrave.java
@@ -14,68 +14,47 @@ import java.util.Scanner;
 import java.util.List;
 
 public class DaveTheBrave {
-//    private Storage storage;
-//    private TaskManager taskManager;
-//    private Ui ui;
-//
-//    public DaveTheBrave(String filePath) {
-//        ui = new Ui();
-//
-//        storage = new Storage(filePath);
-//        taskManager = new TaskManager(storage.loadTasksFromFile(), storage, ui);
-//    }
-//
-//    public void run() {
-//        List<String> greetings = Arrays.asList("hello", "hi", "hey", "yo");
-//        List<String> goodbyes = Arrays.asList("bye", "goodbye");
-//
-//        /*
-//        Scanner object for user input
-//         */
-//        Scanner scanner = new Scanner(System.in);
-//
-//        ui.showWelcome();
-//
-//        while (true) {
-//            /*
-//            Read user input
-//             */
-//            String command = scanner.nextLine();
-//
-//            /*
-//            Exit when user types the command 'bye'
-//             */
-//            if (goodbyes.contains(command.toLowerCase())) {
-//                ui.showGoodbye();
-//                break;
-//            }
-//
-//            /*
-//            Additional greetings
-//             */
-//            if (greetings.contains(command.toLowerCase())) {
-//                ui.respondHello();
-//                continue;
-//            }
-//
-//            // Cheer
-//            if (command.equalsIgnoreCase("cheer")) {
-//                ui.showCheer();
-//                continue;
-//            }
-//
-//            Parser.parseCommand(command, taskManager);
-//        }
-//        /*
-//        Close scanner
-//         */
-//        scanner.close();
-//    }
+    private Storage storage;
+    private TaskManager taskManager;
+    private Ui ui;
+
+    public DaveTheBrave(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        taskManager = new TaskManager(storage.loadTasksFromFile(), storage, ui);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello!");
-//        new DaveTheBrave("data/davethebrave.txt").run();
     }
+
     public String getResponse(String input) {
-        return "Dave: " + input;
+        List<String> greetings = Arrays.asList("hello", "hi", "hey", "yo");
+        List<String> goodbyes = Arrays.asList("bye", "goodbye");
+
+        ui.showWelcome();
+
+        /*
+        Exit when user types the command 'bye'
+         */
+        if (goodbyes.contains(input.toLowerCase())) {
+            return ui.showGoodbye();
+        }
+
+        /*
+        Additional greetings
+         */
+        if (greetings.contains(input.toLowerCase())) {
+            return ui.respondHello();
+        }
+
+        /*
+        Cheer
+         */
+        if (input.equalsIgnoreCase("cheer")) {
+            return ui.showCheer();
+        }
+
+        return Parser.parseCommand(input, taskManager);
     }
 }
--- src/main/java/davethebrave/ui/DialogBox.java
+++ src/main/java/davethebrave/ui/DialogBox.java
@@ -1,26 +1,44 @@
 package davethebrave.ui;
 
-import javafx.beans.Observable;
+import davethebrave.MainWindow;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
-public class DialogBox extends HBox {
+import java.io.IOException;
 
-    private Label text;
+public class DialogBox extends HBox {
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
+    private DialogBox(String s, Image i) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dialog.setText(s);
+        displayPicture.setImage(i);
+    }
+
     private void flip() {
-        this.setAlignment(Pos.TOP_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
         FXCollections.reverse(tmp);
-        this.getChildren().setAll(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
     }
 
     public static DialogBox getUserDialog(String s, Image i) {
@@ -31,18 +49,5 @@ public class DialogBox extends HBox {
         var db = new DialogBox(s, i);
         db.flip();
         return db;
-    }
-
-    public DialogBox(String s, Image i) {
-        text = new Label(s);
-        displayPicture = new ImageView(i);
-
-        //Styling the dialog box
-        text.setWrapText(true);
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
-        this.setAlignment(Pos.TOP_RIGHT);
-
-        this.getChildren().addAll(text, displayPicture);
     }
 }
